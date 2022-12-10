9846bc2ce6ad0fb89977865af34ed54d2a45d9c5
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-10 19:43:15 -0300

    refactor: ensure signUpController.handle implement correct interface

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,9 @@
+import { HttpRequest } from '../protocols/http'
 import { SignUpController } from './signup'
 describe('Signup Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
         email: 'any_email@mail.com',
         password: 'any_password',
@@ -15,7 +16,7 @@ describe('Signup Controller', () => {
   })
   test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
         name: 'any_name@mail.com',
         password: 'any_password',
