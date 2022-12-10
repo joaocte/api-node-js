ef54f01bed211f8287e258847cdf7f03a9dddec6
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-10 20:15:27 -0300

    chore: add verification for fields password and passwordconfirmation

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -19,7 +19,7 @@ describe('Signup Controller', () => {
     const sut = new SignUpController()
     const httpRequest: HttpRequest = {
       body: {
-        name: 'any_name@mail.com',
+        name: 'any_name',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
@@ -27,5 +27,31 @@ describe('Signup Controller', () => {
     const httpRespose = sut.handle(httpRequest)
     expect(httpRespose.statusCode).toBe(400)
     expect(httpRespose.body).toEqual(new MissingParamError('email'))
+  })
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpRespose = sut.handle(httpRequest)
+    expect(httpRespose.statusCode).toBe(400)
+    expect(httpRespose.body).toEqual(new MissingParamError('password'))
+  })
+  test('Should return 400 if no passwordConfirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpRespose = sut.handle(httpRequest)
+    expect(httpRespose.statusCode).toBe(400)
+    expect(httpRespose.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 })
