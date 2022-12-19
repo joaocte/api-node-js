c64ebb78e1199803f34c444e9bf0d22e8bfbb4e7
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2022-12-19 08:13:00 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -16,7 +16,14 @@ describe('EmailValidatorAdapter', () => {
   })
   test('Should return true if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@email.com')
+    const isValid = sut.isValid('valid_email@email.com')
     expect(isValid).toBe(true)
+  })
+  test('Should call validator with correct email', () => {
+    const email = 'valid_email@email.com'
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid(email)
+    expect(isEmailSpy).toHaveBeenCalledWith(email)
   })
 })
